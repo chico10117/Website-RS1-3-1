bd906b3e959faf157a9dbbd1adbc45fc71458caf
¨chico10117¨ <chico10117@gmail.com>
2025-04-14 17:07:38 +0200

    Going back to previous version of SaveButton.svelte

--- src/lib/components/menu-editor/restaurant/QRCode.svelte
+++ src/lib/components/menu-editor/restaurant/QRCode.svelte
@@ -34,7 +34,9 @@
       if (!ctx) return;
 
       const logo = new Image();
-      // Usar una versión PNG del logo
+      // Set desired render size for the SVG before loading
+      logo.width = LOGO_SIZE;
+      logo.height = LOGO_SIZE;
       logo.src = '/favicon.svg'; // Este archivo ya existe en static y es PNG
       
       logo.onload = () => {
@@ -42,15 +44,14 @@
         const x = (QR_SIZE - LOGO_SIZE) / 2;
         const y = (QR_SIZE - LOGO_SIZE) / 2;
         
-        // Crear un fondo blanco más grande para el logo
-        // ctx.fillStyle = 'white';
-        // const bgX = (QR_SIZE - LOGO_BACKGROUND_SIZE) / 2;
-        // const bgY = (QR_SIZE - LOGO_BACKGROUND_SIZE) / 2;
+        ctx.fillStyle = 'white';
+        const bgX = (QR_SIZE - LOGO_BACKGROUND_SIZE) / 2;
+        const bgY = (QR_SIZE - LOGO_BACKGROUND_SIZE) / 2;
         
-        // // Dibujar fondo blanco con bordes redondeados
-        // ctx.beginPath();
-        // ctx.roundRect(bgX, bgY, LOGO_BACKGROUND_SIZE, LOGO_BACKGROUND_SIZE, 10);
-        // ctx.fill();
+        // Dibujar fondo blanco con bordes redondeados
+        ctx.beginPath();
+        ctx.roundRect(bgX, bgY, LOGO_BACKGROUND_SIZE, LOGO_BACKGROUND_SIZE, 10);
+        ctx.fill();
         
         // Dibujar el logo
         ctx.drawImage(logo, x, y, LOGO_SIZE, LOGO_SIZE);
