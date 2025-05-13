b90969895d70326a6100a6125d95783e01a22fdf
Chiko Ubuntu WSL <chico10117@gmail.com>
2025-05-13 12:50:15 +0200

    Adding vercel analytics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,3 +1,9 @@
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit'; // <-- Add this line
+
+// Call injectAnalytics at the module level
+injectAnalytics({ mode: dev ? 'development' : 'production' }); // <-- Add this line
+
 export const prerender = false;
 export const ssr = true;
-export const trailingSlash = 'never'; 
+export const trailingSlash = 'never';
